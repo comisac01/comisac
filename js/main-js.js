be4d404301cb25f1c97b8b7dfa11858a5b7619c1
comisac01 <120954337+comisac01@users.noreply.github.com>
2024-05-29 13:28:57 +0530

    Update main-js.js

--- js/main-js.js
+++ js/main-js.js
@@ -1,86 +1,87 @@
 window.onload = function (e) {
   $(document).ready(function () {
     $('.jumbotron').append(`
-      <div class="row row-content">
-        <div class="mb-0 text-center col-12 col-sm col-md">
-          <span id="jumbo-images1">
-          <i><img src="kiit images/du.jpg" alt="KIITlogo" class="img-fluid jumbotron-img-top"></i> <br><br>
-        <i>   <img src="kiit images/rajguru.png" alt="" class="img-fluid jumbotron-img-bottom"></i>
-           
-          </span>
-          
-        </div>
-        <div class="col-12 col-sm col-md mb-0 text-center">
-          <h1 class="jumbotron-top-heading">INTERNATIONAL SYMPOSIUM ON COMPUTING AND COMMUNICATION SYSTEMS FOR INDUSTRIAL APPLICATIONS(ComSIA)</h1>
-           <!--<h1 style="font-family: 'Merriweather', serif;font-size: x-large;">Theme: Intelligent Computing & Communication Technologies</h1>-->
-          <h1 style="font-family: 'Merriweather', serif;font-size: x-large;">10-11 May 2024</h1>
-          <h2 style="font-family: 'Merriweather', serif;font-size:1.0rem;text-transform:uppercase">ComSIA 2024 will be jointly organsied by School of Open Learning, University of Delhi and
-Shaheed Rajguru College of Applied Sciences, University of Delhi</h2>
-       <!--   <h3 style="font-family: 'Merriweather', serif;font-size:1.0rem;text-transform:uppercase"> In Association With </h3>
-          <hr id="hrr">
-          <h3 style="font-family: 'Merriweather', serif;font-size:1.0rem;text-transform:uppercase">World Leadership Academy</h3>-->
-          <p id="demo" style="font-family: 'Merriweather', serif;font-size:1.4rem">10-11 MAY 2024</p>
-        </div>
-        <div class="col-12 col-sm text-center">
-        <!--<img src="kiit images/springer.png" alt="" class="img-fluid jumbotron-img-bottom">--> <br><br>
-        <!--  <img src="kiit images/CEUR.png" alt="" class="img-fluid jumbotron-img-bottom">-->
-        </div>
-      </div>
+    <span id="jumbo-images1">
+    <i><img src="kiit images/du.jpg" alt="KIITlogo" class="img-fluid jumbotron-img-top rounded-4 rounded box-shadow" width="120"></i> <br><br>
+    <i> <img src="kiit images/rajguru.png" alt="" class="img-fluid jumbotron-img-bottom rounded-4 rounded box-shadow" width="120"></i>
+
+  </span>
+  <div class="jumbo-details">
+    <h1>INTERNATIONAL SYMPOSIUM ON COMPUTING <br>
+      AND COMMUNICATION <br>SYSTEMS FOR INDUSTRIAL APPLICATIONS <br>(ComSIA)</h1>
+    <hr>
+    <h2 class="">
+      May 26-27<sup>th</sup> ,2024,
+    </h2>
+    <h2 class="mb-4">Delhi University, Delhi,India</h2>
+    <p id="demo" style="font-size:1.4rem;font-weight:500"></p>
+  </div>
+  <div class="right-part-container">
+    <div class="right-part box-shadow">
+      <img src="gallery/university.jpg" alt="">
+    </div>
+  </div>
       `);
 
     $('.footer').append(`
-      <div class="container">
-                <div class="row">
-                    <div class="col-4 offset-1 col-sm-2">
-                        <h5> <span class="fa fa-link"></span> Links</h5>
-                        <ul class="list-unstyled">
-                            <li><a href="index.html" style="font-family: 'Merriweather', serif;font-size:1.0rem;">Home</a></li>
-                          <!--  <li><a href="Registrations.html">Registrations</a></li>-->
-                            <li><a href="spon_sorship.html">Sponsorship</a></li>
-                            <li><a href="call_for_papers.html">Call for Papers</a></li>
-                            <li><a href="important_dates.html">Important Dates</a></li>
-                            <li><a href="downloads.html">Downloads</a></li>
-                            <li><a href="https://cmt3.research.microsoft.com/">CMT portal</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-7 col-sm-5">
-                        <h5> <span class="fa fa-map-marker"></span> Address</h5>
-                        <address class="address">
-                       Shaheed Rajguru College of Applied Sciences for Women,<br> Vasundhara Enclave,<br> Delhi-110096, <br> INDIA 
-                      <br>
-                      <!-- <i class="fa fa-phone"> </i> &nbsp;: +9888767262<br>
-          <i class="fa fa-fax"> </i> : +9888767262<br> -->
-          <i class="fa-solid fa-envelope" style="color: #dc3545;"></i> : <a href="mailto:comsia.conf@gmail.com">comsia.conf@gmail.com</a>
-                   </address>
-                    </div>
-                    <div class="col-12 col-sm-4">
-                        <div class="text-center">
-                            <h5 class="text-center">Social Media</h5>
-                            <hr style="border-top: 1px dashed #327FBF">
-                            <a class="btn btn-social-icon btn-facebook" href="#"><i class="fa-brands fa-facebook"></i></a>
-                            <a class="btn btn-social-icon btn-linkedin" href="#"><i class="fa-brands fa-linkedin"></i></a>
-                            <a class="btn btn-social-icon btn-twitter" href="#"><i class="fa-brands fa-twitter"></i></a>
-                            <a class="btn btn-social-icon bg-light" href="mailto:comsia.conf@gmail.com"><i class="fa-solid fa-envelope" style="color: #dc3545;"></i></a>
-                        </div>
-                    </div>
-               </div>
-               </div>
-               <hr color="white">
-               <div class="row justify-content-center">
-                    <div class="col-auto">
-              
-                      <p class="footer_p_i_h2_a footer_p_i_a">Copyright © Intelligent Communication and Computational Research Laboratory|<a href="##"> All Rights Reserved | </a></p>
-                      
-                    </div>
-               </div>
+    <div class="container">
+    <div class="row">
+    <div class="col-4 offset-1 col-sm-2">
+            <h5> <span class="fa fa-link"></span> Links</h5>
+            <ul class="list-unstyled">
+                <li><a href="index.html" class="foot-link">Home</a></li>
+                <li><a href="Registrations.html" class="foot-link">Registrations</a></li>
+                <li><a href="spon_sorship.html" class="foot-link">Sponsorship</a></li>
+                <li><a href="downloads.html" class="foot-link">Downloads</a></li>
+                <li><a href="call_for_papers.html" class="foot-link">Call for Papers</a></li>
+                <li><a href="important_dates.html" class="foot-link">
+                Important Dates</a></li>
+                <li><a href="#" class="foot-link">
+                CMT portal</a></li>
+            </ul>
+        </div>
+        <div class="col-7 col-sm-5 text-center ">
+            <h5> <span class="fa fa-map-marker"></span> Address</h5>
+            <address class="address">
+          <br>
+          <!--<i class="fa fa-phone"> &nbsp;: +</i> <br>--!>
+          <!--<i class="fa fa-fax"> : +</i> <br>--!>
+          <i class="fa fa-envelope fa-lg"></i>  : <a href="mailto:" class="foot-link">email@gmail.com</a>
+       </address>
+        </div>
+        <div class="col-12 col-sm-4 align-self-center">
+            <div class="text-center">
+                <h5 class="text-center">Social Media</h5>
+                <div class="d-flex align-items-center justify-content-center">
+                <hr id="hrr" width="200"/>
+                </div>
+                <a class="btn btn-social-icon btn-facebook mr-2 foot-icon" href="#"><i class="fa fa-facebook fa-lg"></i></a>
+
+                <a class="btn btn-social-icon btn-linkedin mr-2 foot-icon" href="#"><i class="fa fa-linkedin fa-lg"></i></a>
+
+                <a class="btn btn-social-icon btn-twitter mr-2 foot-icon" href="#"><i class="fa fa-twitter fa-lg"></i></a>
+                
+                <a class="btn btn-social-icon bg-light text-primary mr-2 foot-icon" href="mailto:icicc.editor@gmail.com"><i class="fa fa-envelope fa-lg"></i></a>
+            </div>
+        </div>
+        <!--<div class="row justify-content-center">
+          <div class="col-auto">
+
+        <p class="footer_p_i_h2_a footer_p_i_a">Copyright © Intelligent Communication and Computational Research Laboratory|<a href="##"> All Rights Reserved | </a></p>
+        
+            </div>
+            </div>--!>
+      </div>
+   </div>
+</div>
       `);
 
 
     $('.navbar').append(`
-      <button type="button"class="navbar-toggler" data-toggle="collapse" data-target="#Navbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                  <div class="collapse navbar-collapse" id="Navbar" style="justify-content: center;">
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navbar" id="navmenu" onclick="toggleIcon();">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+                                 <div class="collapse navbar-collapse" id="Navbar" style="justify-content: center;">
                     <ul class="nav navbar-nav" style="width: 80%;justify-content: space-evenly;">
                       <li class="nav-item">
                         <a class="nav-link text-white" href="index.html">Home <span class="sr-only">(current)</span></a>
@@ -91,7 +92,7 @@ Shaheed Rajguru College of Applied Sciences, University of Delhi</h2>
                           </a>
                           <div class="dropdown-menu sm-menu text-white">
                           <!-- <a class="dropdown-item text-white" href="about_iccr_lab.html">About ICCRLAB</a> -->
-                            <div class="dropdown-divider"></div>
+                            <!--<div class="dropdown-divider"></div>--!>
                             <a class="dropdown-item text-white" href="about_institute.html">About Institute</a>
                           </div>
                         </li>
@@ -187,9 +188,10 @@ Shaheed Rajguru College of Applied Sciences, University of Delhi</h2>
 
 
 
+
 // me commented
 // Set the date we're counting down to
-var countDownDate = new Date("may 10, 2024 12:00:00").getTime();
+var countDownDate = new Date("june 10, 2024 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -207,8 +209,8 @@ var x = setInterval(function () {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "Days " + ":" + hours + "Hours " + ": "
-    + minutes + "Min" + ": " + seconds + "Sec";
+  document.getElementById("demo").innerHTML = days + "Days " + " : " + hours + "Hours " + " : "
+    + minutes + "Min" + " : " + seconds + "Sec";
 
   // If the count down is over, write some text
   if (distance < 0) {
@@ -217,3 +219,26 @@ var x = setInterval(function () {
   }
 }, 1000);
 document.getElementById('demo').innerHTML = '10-11 MAY 2024';
+
+// navmenu toggle
+function toggleIcon() {
+  const icon = document.querySelector('#navmenu i');
+  if (icon.classList.contains('fa-bars')) {
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark');
+  } else {
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars');
+  }
+}
+
+// dropdown
+$(document).ready(function () {
+  $('.dropdown').hover(function () {
+    $(this).addClass('show');
+    $(this).find('.dropdown-menu').addClass('show');
+  }, function () {
+    $(this).removeClass('show');
+    $(this).find('.dropdown-menu').removeClass('show');
+  });
+});
