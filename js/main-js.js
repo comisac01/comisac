8669e3d5df70e14b2f10c7cdf69409f2cf509cbe
comisac01 <comisac.conf@gmail.com>
2024-05-31 11:27:19 +0530

    changes done in publication file

--- js/main-js.js
+++ js/main-js.js
@@ -11,10 +11,10 @@ window.onload = function (e) {
     <hr>
     <h2 class="">
       March 28-29<sup>th</sup>,2025
-    </h2> </div>
+    </h2> 
     <h3 class="">COMSIA 2025 WILL BE JOINTLY ORGANSIED BY SCHOOL OF <br> OPEN LEARNING, UNIVERSITY OF DELHI AND <br> SHAHEED RAJGURU COLLEGE OF APPLIED SCIENCES, <br> UNIVERSITY OF DELHI <br></h3>
     <p id="demo" style="font-size:1.4rem;font-weight:500"></p>
-  
+  </div>
   <div class="right-part-container">
     <div class="right-part box-shadow">
       <img src="./gallery/university.jpg" alt="">
